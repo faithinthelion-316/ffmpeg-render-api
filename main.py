fc67f5e8e9b4b5272fb26f1169a08e498ba905ca
faithinthelion-316 <faithinthelion@gmail.com>
2026-03-22 19:06:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def build_title_only_filter(numero_regla: str) -> str:
             f"borderw=2:"
             f"bordercolor=black:"
             f"x=(w-text_w)/2:"
-            f"y=h*0.19"
+            f"y=h*0.20"
         ),
         (
             f"drawtext="
@@ -110,7 +110,7 @@ def build_title_only_filter(numero_regla: str) -> str:
             f"borderw=4:"
             f"bordercolor=black:"
             f"x=(w-text_w)/2:"
-            f"y=h*0.24"
+            f"y=h*0.25"
         ),
     ]
 
